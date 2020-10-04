6fda3b1ba992dff25ec969ef808b8857d5059dcf
Murat Onat <murat.onat@me.com>
2020-10-04 22:04:03 +0600

    adding my crmapp

---
+++ crm_main/local_settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
